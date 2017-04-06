7cdfa0d30328bc5f26b8b2b49cb25b5b4f2f0f90
sp <Guest@Epicodus-10D.local>
2017-04-06 13:12:40 -0700

    Write more logic

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,6 @@ var winningCombos = [
 function Board(clickedSquares, availableSquares) {
   this.clickedSquares = clickedSquares;
   this.availableSquares = availableSquares;
-  this.unavailableSquares = [];
 }
 // initialize new board Object
 var newBoard = new Board(0, [0,1,2,3,4,5,6,7,8]);
